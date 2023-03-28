8141bef12f7f949a62cc0b20da20683e50743465
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-03-28 01:26:05 -0600

    Added images for the snake

--- snake.py
+++ snake.py
@@ -13,30 +13,78 @@ font = pygame.font.Font('assets/Fixedsys.ttf', 28)
 background = pygame.transform.scale(background, (window_width, window_height - top_margin))
 length_increment = 1
 key_map = {119: "UP", 115: "DOWN", 97: "LEFT", 100: "RIGHT"}
+images = [pygame.image.load('assets/first-car-right.png').convert_alpha(), #0
+          pygame.image.load('assets/mid-car-right.png').convert_alpha(), #1
+          pygame.image.load('assets/turn-car-right-up.png').convert_alpha(), #2
+          pygame.image.load('assets/turn-car-down-right.png').convert_alpha(), #3
+          pygame.image.load('assets/first-car-left.png').convert_alpha(), #4
+          pygame.image.load('assets/mid-car-left.png').convert_alpha(), #5
+          pygame.image.load('assets/first-car-top-up.png').convert_alpha(), #6
+          pygame.image.load('assets/mid-car-top-up.png').convert_alpha(), #7
+          pygame.image.load('assets/mid-car-top-down.png').convert_alpha(), #8
+          pygame.image.load('assets/first-car-top-down.png').convert_alpha(), #9
+          pygame.image.load('assets/mid-car-top-down.png').convert_alpha(), #10
+          pygame.image.load('assets/turn-car-up-right.png').convert_alpha(), #11
+          pygame.image.load('assets/turn-car-up-left.png').convert_alpha(),] #12
+
+# set the window title
+pygame.display.set_caption("Snake Game")
 
 class Snake:
     def __init__(self, size):
-        self.width = size
-        self.height = size
-        self.snakeArr = [pygame.Vector2(300 - 60, 300 + top_margin), pygame.Vector2(300 - 30, 300 + top_margin), pygame.Vector2(300, 300 + top_margin)]
-        self.length = len(self.snakeArr)
+        self.size = size
+        self.snake_arr = [pygame.Vector2(300 - 60, 300 + top_margin), pygame.Vector2(300 - 30, 300 + top_margin), pygame.Vector2(300, 300 + top_margin)]
+        self.snake_images = [{"pos": pygame.Vector2(size, 0), "img": images[4]}, {"pos": pygame.Vector2(size, 0), "img": images[1]}, {"pos": pygame.Vector2(size, 0), "img": images[0]}]
+        self.length = len(self.snake_arr)
         self.direction = pygame.Vector2(0, 0)
-        self.curr_direction_string = "RIGHT"
+        self.direction_update = pygame.Vector2(0, 0)
         self.direction_queue = []
         self.directions = {"UP": pygame.Vector2(0, -size), "DOWN": pygame.Vector2(0, size), "LEFT": pygame.Vector2(-size, 0), "RIGHT": pygame.Vector2(size, 0)}
-        self.opposite_directions = {"UP": "DOWN", "LEFT": "RIGHT", "DOWN": "UP", "RIGHT": "LEFT"}
         self.collided = False
-    
+        self.images_dictionary = {
+            "RIGHT": images[1],
+            "RIGHT_FIRST": images[0],
+            "RIGHT_LAST": images[4],
+            "LEFT": images[5],
+            "LEFT_FIRST": images[4],
+            "LEFT_LAST": images[0],
+            "UP": images[7],
+            "UP_FIRST": images[6],
+            "UP_LAST": images[8],
+            "DOWN": images[10],
+            "DOWN_FIRST": images[9],
+            "DOWN_LAST": images[6],
+            "RIGHT_UP": images[2],
+            "DOWN_RIGHT": images[3],
+            "LEFT_UP": images[3],
+            "DOWN_LEFT": images[2],
+            "UP_RIGHT": images[11],
+            "UP_LEFT": images[12],
+            "LEFT_DOWN": images[11],
+            "RIGHT_DOWN": images[12],
+        }
+
+    def check_direction(self, direction):
+        if direction == pygame.Vector2(self.size, 0):
+            return "RIGHT"
+        elif direction == pygame.Vector2(-self.size, 0):
+            return "LEFT"
+        elif direction == pygame.Vector2(0, self.size):
+            return "DOWN"
+        else:
+            return "UP"
+
     def change_direction(self, direction):
         direction = direction.upper()
 
         # Check if the directions is opposite to the current snake direction.
-        if direction == self.opposite_directions[self.curr_direction_string]:
+        direction_vector = self.directions[direction]
+        if direction_vector == -self.direction_update:
             return
 
         if direction not in self.direction_queue:
             self.direction_queue.append(direction)
-            self.curr_direction_string = direction
+            self.direction_update = direction_vector
 
     def increase_length(self, points):
         self.length += points
@@ -47,23 +95,36 @@ class Snake:
             del self.direction_queue[0]
 
         if self.direction.x == 0 and self.direction.y == 0:
-            return # If there is no direction then return
+            return
 
         # Generate the new snake head in its corresponding position based on the direction.
-        newHead = self.snakeArr[-1] + self.direction
-        if newHead in self.snakeArr:
+        new_head = self.snake_arr[-1] + self.direction
+        if new_head in self.snake_arr:
             self.collided = True # Check if the snake collided with itself.
 
-        self.snakeArr.append(newHead)
+        self.snake_arr.append(new_head)
+        self.snake_images.append({"pos": self.direction, "img": self.images_dictionary[f"{self.check_direction(self.direction)}_FIRST"]}) # FIRST represents the first car of the train (head of the snake).
 
-        if len(self.snakeArr) > self.length:
-            diff = abs(self.length - len(self.snakeArr))
+        prev_pos = self.snake_images[-2]["pos"]
+        curr_pos = self.snake_images[-1]["pos"]
+        # We check if the snake changed direction.
+        if prev_pos != curr_pos:
+            self.snake_images[-2] = {"pos": self.direction, "img": self.images_dictionary[f"{self.check_direction(prev_pos)}_{self.check_direction(curr_pos)}"]}
+        else:
+            self.snake_images[-2] = {"pos": self.direction, "img": self.images_dictionary[f"{self.check_direction(prev_pos)}"]}
+
+        if len(self.snake_arr) > self.length:
+            diff = abs(self.length - len(self.snake_arr))
             for i in range (0, diff):
-                del self.snakeArr[0]
+                curr_pos = self.snake_images[1]["pos"]
+                self.snake_images[1]["img"] = self.images_dictionary[f"{self.check_direction(curr_pos)}_LAST"] # The last car is added at the beginning of the array.
+                del self.snake_images[0]
+                del self.snake_arr[0]
 
     def draw(self):
-        for body in self.snakeArr:
-            pygame.draw.rect(window, 'black', pygame.Rect(body.x, body.y, self.width, self.height))
+        for index, body in enumerate(self.snake_arr):
+            image = self.snake_images[index]["img"]
+            window.blit(image, (body.x, body.y))
 
 class Food:
     def __init__(self, size, x , y):
@@ -104,28 +165,29 @@ if __name__ == "__main__":
         time_since_last_move = current_time - last_move_time
         if time_since_last_move >= move_interval:
             snake.move()
-            last_move_time = current_time 
+            last_move_time = current_time
+
         # Check if snake ate food.
-        if snake.snakeArr[-1] == food.food_pos:
+        if snake.snake_arr[-1] == food.food_pos:
             food.changePos(random.randint(1, 21) * 30, random.randint(1, 21) * 30 + top_margin)
             snake.increase_length(length_increment)
             score += 1
             text = font.render(f'Score: {score}', True, 'blue')
 
         # Check if the snake is out of bounds.
-        if snake.snakeArr[-1].x > window_width - snake.width:
-            snake.snakeArr[-1].x = 0
-        elif snake.snakeArr[-1].x < 0:
-            snake.snakeArr[-1].x = window_width - snake.width
-        
-        if snake.snakeArr[-1].y > window_height - snake.height:
-            snake.snakeArr[-1].y = 0 + top_margin
-        elif snake.snakeArr[-1].y < top_margin:
-            snake.snakeArr[-1].y = window_height - snake.height
+        if snake.snake_arr[-1].x > window_width - snake.size:
+            snake.snake_arr[-1].x = 0
+        elif snake.snake_arr[-1].x < 0:
+            snake.snake_arr[-1].x = window_width - snake.size
+        if snake.snake_arr[-1].y > window_height - snake.size:
+            snake.snake_arr[-1].y = 0 + top_margin
+        elif snake.snake_arr[-1].y < top_margin:
+            snake.snake_arr[-1].y = window_height - snake.size
 
         # Check if the snake collided with itself.
         if snake.collided:
             running = False
+            break
         
         # Display updates
         window.fill('white')
@@ -135,6 +197,6 @@ if __name__ == "__main__":
         food.draw()
 
         pygame.display.flip()
-        clock.tick(60) # Limit the framerate to 60.
+        clock.tick(300) # Limit the framerate to 60.
 
     pygame.quit()
